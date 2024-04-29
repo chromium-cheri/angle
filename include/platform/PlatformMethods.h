03c71ff469e09f2c36153cb713426d02732bb427
Graeme Jenkinson <graeme@capabilitieslimited.co.uk>
2024-04-29 14:10:09 +0100

    [cheriabi] Disable vulkanImageANGLE.
    This extensions creates a pointer to a `VkImageCreateInfo` structure by
    combining the lo and hi 32bits of a pointer. On CHERI this produces an
    invalid capability.

--- include/platform/PlatformMethods.h
+++ include/platform/PlatformMethods.h
@@ -155,7 +155,11 @@ using AddTraceEventFunc = angle::TraceEventHandle (*)(PlatformMethods *platform,
                                                       int numArgs,
                                                       const char **argNames,
                                                       const unsigned char *argTypes,
+#if defined(__CHERI_PURE_CAPABILITY__)
+                                                      const uintptr_t *argValues,
+#else   // !__CHERI_PURE_CAPABILITY__
                                                       const unsigned long long *argValues,
+#endif  // !__CHERI_PURE_CAPABILITY__
                                                       unsigned char flags);
 inline angle::TraceEventHandle DefaultAddTraceEvent(PlatformMethods *platform,
                                                     char phase,
@@ -166,7 +170,11 @@ inline angle::TraceEventHandle DefaultAddTraceEvent(PlatformMethods *platform,
                                                     int numArgs,
                                                     const char **argNames,
                                                     const unsigned char *argTypes,
+#if defined(__CHERI_PURE_CAPABILITY__)
+                                                    const uintptr_t *argValues,
+#else   // !__CHERI_PURE_CAPABILITY__
                                                     const unsigned long long *argValues,
+#endif  // !__CHERI_PURE_CAPABILITY__
                                                     unsigned char flags)
 {
     return 0;
